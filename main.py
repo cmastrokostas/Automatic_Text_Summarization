59f21d10b8b29eb4d1406a665e38cc8465bd5a97
Charalampos <53084462+cmastrokostas@users.noreply.github.com>
2022-04-30 21:16:31 +0300

    Changed final json file format .

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ from SumSurvey.config import en_path, el_path
 def main():
 
     languages= {1: el_path, 2: en_path}
+
     for lang in languages : 
         summarization(languages[lang])
-    #while True:
-    #    inp = int(input("Choose '1' for Greek or '2' for English texts : "))
-    #    if (inp == 1 or inp == 2 ):
-    #        print(summarization(languages[inp]))
-    #        break
-    #    else :
-    #        print("Try Again...")
+
     print("Done...")
     return
 
